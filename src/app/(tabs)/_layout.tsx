3f773c6de81c48f39ea66d3899bee8d6f88e41d0
kislay-gupta <kislaygupta007@gmail.com>
2024-10-16 00:44:33 +0530

    installed expo router and setup tab navigation

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "For you",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="new"
+        options={{
+          title: "Create Post",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="plus-square-o" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile ",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={26} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/app/_layout.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function Rootlayout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
